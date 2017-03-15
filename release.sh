379fc0e437c30d7d98f50433d98d4a0914d1ae84
Kevin Glinski <kevin.glinski@inin.com>
2017-03-15 15:55:16 -0400

    changing index file dir

--- release.sh
+++ release.sh
@@ -6,4 +6,4 @@ export PATH=$PATH:/home/jenkins/bin
 aws s3 cp $WORKSPACE/repo/localBuild/ s3://inin-static-assets/$S3_STATIC_ASSETBUCKET/$BUILD_NUMBER --recursive --exclude "*.svg" --acl "public-read"
 #content type of svg needs to be explicitly set
 aws s3 cp $WORKSPACE/repo/localBuild/ s3://inin-static-assets/$S3_STATIC_ASSETBUCKET/$BUILD_NUMBER --recursive --exclude "*" --include "*.svg" --acl "public-read" --content-type "image/svg+xml"
-aws s3 sync s3://inin-static-assets/$S3_STATIC_ASSETBUCKET/$BUILD_NUMBER s3://inin-index-files-dev/developercenter --include "*" --acl "public-read" --cache-control "max-age=0, no-cache, no-store" --metadata-directive REPLACE --delete
+aws s3 sync s3://inin-static-assets/$S3_STATIC_ASSETBUCKET/$BUILD_NUMBER s3://inin-index-files-dev/developercenter-blog/blog --include "*" --acl "public-read" --cache-control "max-age=0, no-cache, no-store" --metadata-directive REPLACE --delete
