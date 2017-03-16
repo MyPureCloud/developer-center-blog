88d29c8ad008b3e9b7cce165c2e1f710b95cce98
Kevin Glinski <kevin.glinski@inin.com>
2017-03-16 15:05:28 -0400

    fix less build

--- config.rb
+++ config.rb
@@ -95,9 +95,6 @@ page "/feed.xml", layout: false
 
 # Build-specific configuration
 configure :build do
-  # Minify CSS on build
-  # activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
+    ignore /stylesheets\/(?!styles).*\.less/
+    ignore /stylesheets\/lib\/less\/*/
 end
