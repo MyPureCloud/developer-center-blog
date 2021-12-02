aa375c2832e82d2f3fd4cd6f48f1f25db8338847
Charlie Conneely <87772562+charliecon@users.noreply.github.com>
2021-12-02 18:27:59 +0000

    DEVENGAGE-798: Vault with CX as Code (#97)
    * DEVENGAGE-798: Vault with CX as Code
    
    * DEVENGAGE-798: Made suggested changes to index.md

---
+++ source/2021-11-25-vault-with-cx-as-code/main.tf
@@ -0,0 +1,28 @@
+terraform {
+    required_version = ">= 0.12"
+    required_providers {
+        genesyscloud = {
+            source  = "mypurecloud/genesyscloud"
+            version = "0.14.0"
+        }
+    }
+}
+
+provider "vault" {
+    # address defaults to $VAULT_ADDR
+    # token defaults to $VAULT_TOKEN
+}
+
+provider "genesyscloud" {
+    oauthclient_id     = data.vault_generic_secret.genesys_client_credentials.data["client_id"]
+    oauthclient_secret = data.vault_generic_secret.genesys_client_credentials.data["client_secret"]
+    aws_region         = data.vault_generic_secret.genesys_client_credentials.data["region"]
+}
+
+resource "genesyscloud_routing_wrapupcode" "example" {
+    name = "Test WUC"
+}
+
+data "vault_generic_secret" "genesys_client_credentials" {
+    path = "secret/genesyscloud_creds"
+}
